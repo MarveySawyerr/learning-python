62d7aeb3bc354fa80666a82eef5856e2bd48a4cc
Marvellous Sawyerr <marveldesigns98@gmail.com>
2026-03-31 01:38:59 +0100

    Day 1: Variables completed

---
+++ 01_variables.py
@@ -0,0 +1,9 @@
+#Day 1: Creating variables and assigning values to them
+customer_name = input('Your name is ') #customer inputs name
+print('Welcome back', customer_name)
+bank_name = 'Marvel Bank'
+available_balance = 30340.25
+bank_branch = 'New York'
+is_active = True
+#Return customer details and balance
+print('Welcome to', bank_name + ',', customer_name + '. Your available balance is $', available_balance)
